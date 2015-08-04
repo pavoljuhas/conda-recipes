1890b1fb28da6691d08e12b730677d5efd6ebc0a
Pavol Juhas <pavol.juhas@gmail.com>
2015-08-04 18:09:46 -0400

    Add recipe for pyobjcryst-1.0 patch 4000dab.
    Use the default boost from Anaconda and our libobjcryst package.

---
+++ pyobjcryst/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export CPATH="$PREFIX/include:$CPATH"
+export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+
+if [ `uname` == Darwin ]; then
+    export DYLD_FALLBACK_LIBRARY_PATH="$PREFIX/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+fi
+
+scons -j $CPU_COUNT install prefix=$PREFIX
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
