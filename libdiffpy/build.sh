bcbcd6522f1513f25c0fc3a2d458bef0bc7d60a3
Pavol Juhas <pavol.juhas@gmail.com>
2015-09-04 17:33:52 -0400

    Add libdiffpy 1.2.72.

---
+++ libdiffpy/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export CPATH="${PREFIX}/include:${CPATH}"
+export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+
+scons -j $CPU_COUNT lib alltests
+
+# Execute unit tests in a modified environment so that Anaconda
+# shared libraries can be found.
+
+if [ `uname` == Darwin ]; then
+    DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib" scons test
+else
+    LD_LIBRARY_PATH="${PREFIX}/lib" scons test
+fi
+
+scons install prefix=$PREFIX
+
+grep '^#define DIFFPY_VERSION_STR' < "${PREFIX}/include/diffpy/version.hpp" |
+    cut -d '"' -f 2 > __conda_version__.txt
