e33bda83676821f7ad6b9aafc32d58914919eef1
Pavol Juhas <pavol.juhas@gmail.com>
2015-09-10 17:38:41 -0400

    libdiffpy - avoid binary relocation on Linux.
    Configure RPATH in the build script instead.  patchelf seems
    to produce shared library that cannot be reliably linked with.

--- libdiffpy/build.sh
+++ libdiffpy/build.sh
@@ -2,6 +2,9 @@
 
 export CPATH="${PREFIX}/include:${CPATH}"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+if [[ `uname` == Linux ]]; then
+    export LDFLAGS="${LDFLAGS} "'-Wl,-rpath,\\$$ORIGIN/.'
+fi
 MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
 
 scons -j $MYNCPU lib alltests
@@ -9,7 +12,7 @@ scons -j $MYNCPU lib alltests
 # Execute unit tests in a modified environment so that Anaconda
 # shared libraries can be found.
 
-if [ `uname` == Darwin ]; then
+if [[ `uname` == Darwin ]]; then
     DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib" scons test
 else
     LD_LIBRARY_PATH="${PREFIX}/lib" scons test
