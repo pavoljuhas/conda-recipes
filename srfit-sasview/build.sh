ab9485d25a0268fc2f6b9ad79c6705d09caed674
Pavol Juhas <pavol.juhas@gmail.com>
2015-10-23 14:58:57 -0400

    Add recipe for srfit-sasview 3.1.1.
    Derived from SasView release 3.1.1,
    SasView/sasview@225aca8aa196419ca220ac57c9a3934bbbe0fbcc
    
    The srfit-sasview package includes the following SasView modules:
    sas.data_util, sas.dataloader, sas.models, sas.pr.

---
+++ srfit-sasview/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$PYTHON setup.py easy_install --no-deps --exclude-scripts .
+
+# Add more build steps here, if they are necessary.
+
+# See # http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
