4ef7336f73019d3ecf8f262d7d3ae3931b40e9d2
Pavol Juhas <pavol.juhas@gmail.com>
2017-05-10 18:09:57 -0400

    pyobjcryst 2.0.2
    Sync with diffpy/pyobjcryst@6308e35e6e4dd3be5997de6370458954a1aacadc

--- pyobjcryst/build.sh
+++ pyobjcryst/build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-export CPATH="$PREFIX/include:$CPATH"
-export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
+export CPATH="${PREFIX}/include:$CPATH"
+export LIBRARY_PATH="${PREFIX}/lib:$LIBRARY_PATH"
+
+MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
 
 if [ `uname` == Darwin ]; then
-    export DYLD_FALLBACK_LIBRARY_PATH="$PREFIX/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
 fi
 
+# Apply sconscript.local customizations.
+cp ${RECIPE_DIR}/sconscript.local ./
+
+# Install package with scons to utilize multiple CPUs.
 scons -j $MYNCPU install prefix=$PREFIX
 
 # Add more build steps here, if they are necessary.
 
-$PYTHON setup.py --version > __conda_version__.txt
-
-# See
-# http://docs.continuum.io/conda/build.html
+# See http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
